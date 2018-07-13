3ecd00a861b25bc18e4ed7b61d371c90b4f96a34
ezngo <ezngo.jyp@iezngo.com>
2018-07-13 10:07:05 +0900

    first commit

---
+++ ml/static/ml/bower_components/webvideo/webvideo.js
@@ -0,0 +1,25 @@
+alert(2);
+(function() {
+
+    var video, localMediaStream;
+
+    window.URL = window.URL || window.webkitURL;
+    navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    video = document.querySelector('video');
+
+    document.querySelector('button#capture').addEventListener('click', function () {
+        navigator.getUserMedia({audio: true, video: true}, function(stream) {
+            localMediaStream = stream;
+            video.src = window.URL.createObjectURL(stream);
+        }, function(e) {
+            console.log(e);
+        });
+    });
+
+    document.querySelector('button#stop').addEventListener('click', function(e) {
+        video.pause();
+        localMediaStream.stop();
+    });
+}());
